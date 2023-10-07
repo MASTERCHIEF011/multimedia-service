cc57e0345f2937fadf763c205d042af010bf6626
Harsh Pandey <harshpandey011@gmail.com>
2023-10-07 20:54:22 +0530

    initial commit

---
+++ src/main/java/com/insights/multimediaservice/MultimediaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.insights.multimediaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultimediaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultimediaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insights/multimediaservice/MultimediaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insights.multimediaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultimediaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
